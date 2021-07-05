124f7742949c2f9c57c1e354da3f6232cdafa934
nebiyu <83504562+tnebiyu@users.noreply.github.com>
2021-07-05 16:09:15 +0300

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,100 @@
+public class MyClass {
+    private double width;
+
+    private double height;
+
+
+
+    public  MyClass(){
+
+        this(0,0);
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    public MyClass(double width,double height){
+
+        this.width=width;
+
+        this.height=height;
+
+        if(width<0){
+
+            this.width=0;
+
+        }
+
+        if(height<0){
+
+            this.height=0;
+
+        }
+
+    }
+
+
+
+    public double getWidth() {
+
+        return width;
+
+    }
+
+
+
+    public void setWidth(double width) {
+
+        if(width<0){
+
+            this.width=0;
+
+        }
+
+        else{
+
+            this.width = width;
+
+
+
+        }
+
+    }
+
+
+
+    public double getHeight() {
+
+        return height;
+
+    }
+
+
+
+    public void setHeight(double height) {
+
+        if (height < 0) {
+
+            this.height = 0;
+
+        } else {
+
+            this.height = height;
+
+
+
+        }
+
+    }
+
+    public double getArea(){
+
+        double wallArea=this.width*this.height;
+
+        return wallArea;
+
+    }
+}
+
